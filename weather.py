6486429cf63e7fad3544375be6c307ffba19b51d
majickdave <majickdave@gmail.com>
2021-10-08 14:46:13 -0400

    added requirements

--- weather.py
+++ weather.py
@@ -12,27 +12,27 @@ app = Flask(__name__)
 API_KEY = '17afee29d93a1db02dda0f1817e0aca1'
 
 # get weather by U.S. zip code
-API_URL = ('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude={}&units=imperial&appid={}')
+API_URL = ('https://api.openweathermap.org/data/2.5/onecall?lat=39.40538934466007&lon=-76.70943148008318&exclude=(hourly,%20minutely)&units=imperial&appid={}')
 
-def query_api(lat, lon, exclude):
+def query_api():
     """submit the API query using variables for zip and API_KEY"""
     try:
         # print(API_URL.format(zip, API_KEY))
-        data = requests.get(API_URL.format(lat, lon, exclude, API_KEY)).json()
+        data = requests.get(API_URL.format(API_KEY)).json()
     except Exception as exc:
         print(exc)
         data = None
     return data
     
-@app.route('/weather/<lat>:<lon>:<exclude>')
-def result(lat, lon, exclude):
+@app.route('/weather')
+def result():
     """
     options for exclude:
     [current, minutely, hourly, daily, alerts]
     http://127.0.0.1:5000/weather/39.40538934466007:-76.70943148008318:(hourly,%20minutely)
     """
     # get the json file from the OpenWeather API
-    resp = query_api(lat, lon, exclude)
+    resp = query_api()
     # construct a string using the json data items for temp and
     # description
     try:
